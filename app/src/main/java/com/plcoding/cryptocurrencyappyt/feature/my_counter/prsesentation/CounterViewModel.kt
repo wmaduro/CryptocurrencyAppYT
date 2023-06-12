1f095ed5e79258249fa820d35bbdab744118e446
Welerson Maduro <welerson.maduro@foodstuffs-si.co.nz>
2023-06-13 06:29:09 +1200

    collectLatest

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/feature/my_counter/prsesentation/CounterViewModel.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/feature/my_counter/prsesentation/CounterViewModel.kt
@@ -1,13 +1,19 @@
 package com.plcoding.cryptocurrencyappyt.feature.my_counter.prsesentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CounterViewModel  @Inject constructor(): ViewModel() {
+
+
     val countDownFlow = flow <Int>{
         val startValue = 10
         var currValue = startValue
@@ -19,4 +25,18 @@ class CounterViewModel  @Inject constructor(): ViewModel() {
             emit(currValue)
         }
     }
+
+    init {
+        collectTest()
+    }
+   private fun collectTest(){
+        viewModelScope.launch {
+            countDownFlow.collectLatest {  time ->
+                println("lixo waiting $time")
+                delay(3000)
+                println("lixo $time")
+            }
+        }
+    }
 }
+
